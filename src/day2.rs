555c3d8ebb0318e5ab0b46d3fbe8d7acc3fa3559
megalphian <megnath@ualberta.ca>
2019-12-09 22:25:18 -0700

    Gets arguments with parameter type

--- src/day2.rs
+++ src/day2.rs
@@ -1,15 +1,16 @@
 use std::fs;
 mod intcode;
 use intcode::intcode::IntCodeMemory;
-use intcode::intcode::ProgramNounVerb;
+use intcode::intcode::IntCodeCompute;
 
-fn search_protocol(int_code : IntCodeMemory, final_val: i32) -> i32
+fn search_protocol(memory_values : &Vec<i32>, final_val: i32) -> i32
 {
     let mut out_protocol : i32 = 0;
 
     for i in 0..99
     {
         for j in 0..99 {
+            let mut int_code = IntCodeMemory::new(memory_values);
             let result = int_code.run_opcode_instructions(i, j);
             if final_val == result {
                 out_protocol = 100 * i + j;
@@ -30,9 +31,9 @@ fn main() {
                                     .map(|s| s.trim().parse().unwrap())
                                     .collect();
 
-    let int_code = IntCodeMemory::new(&memory_values);
-    let part1_result = int_code.run_opcode_instructions(12,2);
-    let part2_result = search_protocol(int_code, 19690720);
+    let mut int_code1 = IntCodeMemory::new(&memory_values);
+    let part1_result = int_code1.run_opcode_instructions(12,2);
+    let part2_result = search_protocol(&memory_values, 19690720);
 
     println!("1202 result = {}", part1_result);
     println!("19690720 result = {}", part2_result);
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,54 +1,84 @@
 pub mod intcode {
 
-    pub trait ProgramNounVerb {
-        fn run_opcode_instructions(&self, noun : i32, verb : i32) -> i32;
+    pub trait IntCodeCompute {
+        fn run_opcode_instructions(&mut self, noun : i32, verb : i32) -> i32;
+        fn run_program(&mut self) -> i32;
+        fn get_arguments(&self, _index : usize) -> Vec<i32>;
     }
 
     pub struct IntCodeMemory {
-        _command_list : Vec<i32>
-    }
-
-    fn run_program(mut cmds: Vec<i32>) -> i32 {
-        for x in (0..cmds.len() - 1).step_by(4){
-            match cmds[x] {
-                99 => break,
-                1 => {
-                    let out_pos = cmds[x+3];
-                    let in_pos_1 = cmds[x+1];
-                    let in_pos_2 = cmds[x+2];
-                    cmds[out_pos as usize] = cmds[in_pos_1 as usize] + cmds[in_pos_2 as usize]
-                }
-                2 => {
-                    let out_pos = cmds[x+3];
-                    let in_pos_1 = cmds[x+1];
-                    let in_pos_2 = cmds[x+2];
-                    cmds[out_pos as usize] = cmds[in_pos_1 as usize] * cmds[in_pos_2 as usize]
-                }
-                _ => panic!("Unrecognized opcode")
-            }
-        }
-
-        cmds[0]
+        command_list : Vec<i32>
     }
 
     impl IntCodeMemory {
-        pub fn new(_command_list : &Vec<i32>) -> IntCodeMemory {
+        pub fn new(command_list : &Vec<i32>) -> IntCodeMemory {
             IntCodeMemory {
-                _command_list: _command_list.to_vec()
+                command_list: command_list.to_vec()
             }
         }
     }
 
-    impl ProgramNounVerb for IntCodeMemory {
+    impl IntCodeCompute for IntCodeMemory {
 
-        fn run_opcode_instructions(&self, noun : i32, verb : i32) -> i32 {
+        fn run_opcode_instructions(&mut self, noun : i32, verb : i32) -> i32 {
+
+            self.command_list[1] = noun;
+            self.command_list[2] = verb;
+        
+            self.run_program()
+        }
+
+        fn run_program(&mut self) -> i32 {
+            let mut x : usize = 0;
+            loop{
+                match self.command_list[x] % 100 {
+                    99 => break,
+                    01 => {
+                        let out_pos = self.command_list[x+3];
+                        let in_args = self.get_arguments(x);
+                        self.command_list[out_pos as usize] = in_args[0] + in_args[1];
+                        x += 4;
+                    }
+                    02 => {
+                        let out_pos = self.command_list[x+3];
+                        let in_args = self.get_arguments(x);
+                        self.command_list[out_pos as usize] = in_args[0] * in_args[1];
+                        x += 4;
+                    }
+                    03 => {
     
-            let mut cached_command_list : Vec<i32> = self._command_list.clone();
-            cached_command_list[1] = noun;
-            cached_command_list[2] = verb;
-        
-            run_program(cached_command_list)
-        
+                        x += 2;
+                    }
+                    _ => panic!("Unrecognized opcode {} {}", self.command_list[x], x)
+                }
+            }
+
+            self.command_list[0]
+        }
+
+        fn get_arguments(&self, _index : usize) -> Vec<i32> {
+            
+            let op_code = self.command_list[_index] as usize;
+            let mut return_value : Vec<i32> = [0,0].to_vec();
+            let temp1 = self.command_list[_index + 1];
+            let temp2 = self.command_list[_index + 2];
+            if ((op_code % 1000) / 100) == 1 {
+                return_value[0] = temp1;
+            }
+            else
+            {
+                return_value[0] = self.command_list[temp1 as usize];
+            }
+
+
+            if (op_code) / 1000 == 1 {
+                return_value[1] = temp2;
+            }
+            else {
+                return_value[1] = self.command_list[temp2 as usize];
+            }
+
+            return_value
         }
     }
 }
